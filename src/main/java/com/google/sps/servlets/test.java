1c28cd9da7bf1fc8d4a2df77d519f92c8fc35699
Waldemar Ramos Colon <ramoswaldemar082@gmail.com>
2021-07-15 21:36:36 +0000

    Fix format

--- src/main/java/com/google/sps/servlets/test.java
+++ src/main/java/com/google/sps/servlets/test.java
@@ -11,48 +11,45 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONObject;
 
 @WebServlet("/test")
-public class test extends HttpServlet
-{
-  
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
-  {
-       Connection conn; // Cloud SQL connection 
-      try
-      {
-          conn = DriverManager.getConnection("jdbc:mysql:///team5project?cloudSqlInstance=wramos-sps-summer21:us-central1:team5project&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=spsTeam5&useSSL=false"); 
-          ResultSet rs = conn.prepareStatement("SELECT * FROM QUESTIONS").executeQuery(); 
-          List<JSONObject> resList = new ArrayList<JSONObject>();
+public class test extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Connection conn; // Cloud SQL connection
         try {
-            // get column names
-            ResultSetMetaData rsMeta = rs.getMetaData();
-            int columnCnt = rsMeta.getColumnCount();
-            List<String> columnNames = new ArrayList<String>();
-            for(int i=1;i<=columnCnt;i++) {
-                columnNames.add(rsMeta.getColumnName(i).toUpperCase());
-            }
-
-            while(rs.next()) { // convert each object to an human readable JSON object
-                JSONObject obj = new JSONObject();
-                for(int i=1;i<=columnCnt;i++) {
-                    String key = columnNames.get(i - 1);
-                    String value = rs.getString(i);
-                    obj.put(key, value);
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql:///team5project?cloudSqlInstance=wramos-sps-summer21:us-central1:team5project&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=spsTeam5&useSSL=false");
+            ResultSet rs = conn.prepareStatement("SELECT * FROM QUESTIONS").executeQuery();
+            List<JSONObject> resList = new ArrayList<JSONObject>();
+            try {
+                // get column names
+                ResultSetMetaData rsMeta = rs.getMetaData();
+                int columnCnt = rsMeta.getColumnCount();
+                List<String> columnNames = new ArrayList<String>();
+                for (int i = 1; i <= columnCnt; i++) {
+                    columnNames.add(rsMeta.getColumnName(i).toUpperCase());
                 }
-                resList.add(obj);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-          response.setContentType("application/json;"); 
-          response.getWriter().println(resList);
 
-      }catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-      finally{
-          //Nothing here
-      }
-  }
+                while (rs.next()) { // convert each object to an human readable JSON object
+                    JSONObject obj = new JSONObject();
+                    for (int i = 1; i <= columnCnt; i++) {
+                        String key = columnNames.get(i - 1);
+                        String value = rs.getString(i);
+                        obj.put(key, value);
+                    }
+                    resList.add(obj);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            response.setContentType("application/json;");
+            response.getWriter().println(resList);
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            // Nothing here
+        }
+    }
 }
